65b0c54b07e0056815ea07c0ca050703d46d9385
toh5148 <toh5148@psu.edu>
2016-03-11 16:45:05 -0500

    a

---
+++ botbattle/assets/js/playback.js
@@ -0,0 +1,30 @@
+﻿/*  Functions in here are used to control the playback
+    options of playback mode within a display session */
+
+function GoToStart() {
+
+}
+
+function StepBackOneTurn() {
+
+}
+
+function Pause() {
+
+}
+
+function Play(speed) {
+
+}
+
+function StepForwardOneTurn() {
+
+}
+
+function GoToEnd() {
+
+}
+
+function ChangePlaybackSpeed(newSpeed) {
+
+}
