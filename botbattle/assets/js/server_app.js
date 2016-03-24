f89b3fc744395bc07650a8bde0e5c96b181fb623
seb5646 <seb5646@psu.edu>
2016-03-24 12:04:25 -0400

    server can now query the database for a match id

--- botbattle/assets/js/server_app.js
+++ botbattle/assets/js/server_app.js
@@ -2,11 +2,12 @@
 var mysql = require("mysql");
 
 // Create a connection variable
-var con = mysql.createConnection({
+var con = mysql.createConnection(
+{
   host: "hbgwebfe.hbg.psu.edu",
   user: "bbweb",
   password: "sun16event",
-  database: ""
+  database: "bbweb"
 });
 
 // Try and connect to the database
@@ -18,8 +19,39 @@ con.connect(function(err){
   console.log('Connection established');
 });
 
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-});
+/*var str = "[ { command:\"test game instance, please delete later\" } ]";
+var game_instance = { id: 105, command: str, winner_id: 20};
+con.query('INSERT INTO match_turns SET ?', game_instance, function(err,res){
+  if(err) throw err;
+
+  console.log('Last insert ID:', res.insertId);
+});*/
+
+function sendRequest(id){
+	// column names: id, command, winner_id
+	con.query('SELECT command FROM match_turns  WHERE id = ?', id, function(err, rows){
+		if(err) {
+			alert("Match id not found");
+			console.log("Match id:" + id + " not found.");
+			return false;
+		}
+
+		console.log('Data received from Db:\n');	
+		// Turn the text recieved into a JSON object
+		var match_commands = JSON.parse(rows[0].command);
+		console.log(JSON.stringify(match_commands));
+		
+		return match_commands;		
+	});
+}
+
+function closeConnection(){
+	con.end(function(err) {
+	  // The connection is terminated gracefully
+	  // Ensures all previously enqueued queries are still executed
+	  // before sending a COM_QUIT packet to the MySQL server.
+	});
+}
+
+var x = sendRequest(105);
+closeConnection();
