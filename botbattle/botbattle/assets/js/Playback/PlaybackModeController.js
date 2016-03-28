f4006df9245c084560baeb1b23b6ceac107f7fc9
toh5148 <toh5148@psu.edu>
2016-03-28 14:10:44 -0400

    Cleaned up playback and testing arena code/file structure

--- botbattle/botbattle/assets/js/Playback/PlaybackModeController.js
+++ botbattle/botbattle/assets/js/Playback/PlaybackModeController.js
@@ -1,73 +1,29 @@
-﻿/*  Functions in here are used to control the playback
-    options of playback mode within a display session */
-
-function GoToStart() {
-    /*If we're storing the entire set of moves, we should just be 
-    able to grab the 'Game Initialization Message' to line pieces up */
-	restoreGameState(0);
-}
-
-function StepBackOneTurn() {
-    /* We'll probably have to start at the beginning of the match
-    and keep track of the last position of each entity up until
-    one turn before the current turn and make those adjustments */
-	restoreGameState(turn-1);
-}
-
-function Pause() {
-    /* Should just stop updating the rendering */
-	playing=false;
-}
-
-function Play() {
-    /* Should be able to just start playing from current position
-    (turn number) at the speed adjustment given as a multiplier */
-	playing=true;
-}
-
-function StepForwardOneTurn() {
-    /* Make adjustments for the next turn and pause at the start */
-	restoreGameState(turn+1);
-}
-
-function GoToEnd() {
-    /* Go to the end of the game and show the final positions
-    of each of the entities */
-	restoreGameState(turns.length);
-}
-
-function ChangePlaybackSpeed(newSpeed) {
-    /* Change the playback speed of this 'class'...should continue
-    in whichever mode (play/paused) that it was in before */
-	playbackSpeed=newSpeed;
-}
-
-
-/* The rest of these functions are to retrieve match data from the 
-    server and set the appropriate variables for the GDM to run correctly */
-
-function retrieveMatch() {
+﻿function retrieveMatch() {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
     var matchID = -1;
     for (var i=0;i<vars.length;i++) {
         var pair = vars[i].split("=");
-        if(pair[0] == "matchid") {
+        if(pair[0] == "mid") {
             matchID = pair[1];
             break;
         }
     }
     
-    //Take care of query string errors
+    //Handle query string errors
     if (matchID == -1) {
         //No match id was provided in the query string
         /*Redirect them to an error page with links and
           possible causes of error */
-        window.location.href = "error/playbackerror.html";
+        sendError("?mes=p1");
     }
     else {
         matchRequest(matchID);
     }
+}
+
+function sendError(qString) {
+    window.location.href = "error.html" + qString;
 }
 
 // Function to get an XMLHttpRequest that will be used to send an ajax call
