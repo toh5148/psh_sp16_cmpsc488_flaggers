4a7ef375ff47f7eae9ea28fe805293d04c983b49
tus193 <tahahsiddique@gmail.com>
2016-03-30 14:25:09 -0400

    started bot upload

--- botbattle/server code/server.js
+++ botbattle/server code/server.js
@@ -7,7 +7,7 @@ var credentials = require('credentials');
 var app = express();
 var port = 5050;
 var numAttempts = 0; // number of ties we tried to connect to the db and failed
-var db; // connection variable
+var con; // connection variable
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -267,7 +267,7 @@ x = JSON.stringify(x);*/
 
 /*openConnection();
 var game_instance = { id: 12345, command: x, winner_id: 1};
-db.query('INSERT INTO match_turns SET ?', game_instance, function(err,res){
+con.query('INSERT INTO match_turns SET ?', game_instance, function(err,res){
   if(err) throw err;
 
   console.log('row inserted');
@@ -276,14 +276,14 @@ db.query('INSERT INTO match_turns SET ?', game_instance, function(err,res){
 
 // Opens the connection to the database, throws an error if connection failed.
 function openConnection() {
-	db = mysql.createConnection(
+	con = mysql.createConnection(
 	{
 	  host: credentials.host,
 	  user: credentials.user,
 	  password: credentials.password,
 	  database: credentials.database
 	});
-	db.connect(function(err){
+	con.connect(function(err){
 	  if(err){
 		console.log('Error connecting to the ' + credentials.database + ' database.');
 		throw err;
@@ -295,7 +295,7 @@ function openConnection() {
 
 // Closes the connection to the database, throws error if closing the connection failed
 function closeConnection() {
-	db.end(function(err) {
+	con.end(function(err) {
 	  // The connection is terminated gracefully
 	  // Ensures all previously enqueued queries are still executed
 	  // before sending a COM_QUIT packet to the MySQL server.
@@ -311,7 +311,7 @@ function closeConnection() {
 /* 
 	This function queries the database for a specific match id
 	Arguments:
-		id - Match id, callback function
+		id - Match id
 		
 	When query finishes, call the 'callback' function giving it the result
 */
@@ -319,14 +319,13 @@ function getMatch(id, callback){
 	var retval;
 	openConnection();
 	// column names: match_id, game_initialization_message, turns, ready_for_playback
-	// will need to do something with the playback_ready field
-	//db.query('SELECT game_initialization_message, turns FROM matches WHERE match_id = ?', id, function(err, rows){
-	db.query('SELECT command FROM match_turns WHERE id = ?', id, function(err, rows){
+	//con.query('SELECT game_initialization_message, turns FROM matches WHERE match_id = ?', id, function(err, rows){
+	con.query('SELECT command FROM match_turns WHERE id = ?', id, function(err, rows){
 		if(err) { // error with the database
-			retval = 'false';
+			retval = false;
 		} else if (rows[0] == undefined){ // Match does not exists
 			console.log('ERROR: Match with id:' + id + ' not found.');
-			retval = 'null';					
+			retval = null;					
 		} else{ // Match exist
 			//console.log(rows[0].command);
 			
@@ -349,7 +348,7 @@ function getMatch(id, callback){
 function getTestInstance(uid, cid, callback){
 	var retval;
 	// column names: uid, challenge_id, game_initialization_message, turns
-	db.query('SELECT command FROM testarena_turns WHERE challenge_id = ?', id, function(err, rows){
+	con.query('SELECT command FROM testarena_turns WHERE challenge_id = ?', id, function(err, rows){
 		if(err) {
 			retval = false;
 		}		
@@ -363,8 +362,31 @@ function getTestInstance(uid, cid, callback){
 			retval = rows[0].command;
 		}
 		callback(retval); // send the result
-	});
+	}); 
 }
+
+/*
+
+function storeBot(botText, callback){
+	
+	
+	var retval;
+	// column names: 
+	openConnection();
+	var bot = { id: 12345, command: x, winner_id: 1};
+	con.query('INSERT INTO Bot SET ?', bot, function(err,res){
+  	if(err) throw err;
+
+  	console.log('row inserted');
+  	closeConnection();
+}
+
+
+
+*/
+
+
+
 
 // THE FOLLOWING URLS WILL PROBABLY CHANGE ALONG
 // WITH THE PARAMETERS
@@ -387,13 +409,25 @@ app.get('/get_test_instance', function(req, res, next){
 	var msg = getTestInstance(user_id, function(data){
 		console.log('server sent: ' + data);
 		res.header('Access-Control-Allow-Origin', '*');
-		res.send(data);
+		res.send(data); 
 	});
 });
 
+
+/*
+
+// localhost:5050/storeBot
+app.get('/storeBot', function(req, res, next){
+	
+
+}
+
+*/
+
+
 // localhost:5050/openDB
 app.get('/openDB', function(req, res, next){
-	try {
+	try { 
 		openConnection();
 		res.send(true);
 	} catch (err) {
@@ -405,7 +439,7 @@ app.get('/openDB', function(req, res, next){
 // localhost:5050/closeDB
 app.get('/closeDB', function(req, res, next){
 	try {
-		closeConnection();
+		closeConnection(); 
 		res.send(true);
 	} catch (err) {
 		console.log('Error terminating the connection.');
