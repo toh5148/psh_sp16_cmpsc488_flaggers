05a668d5a504b120b1380c9886581e97e45d9a53
toh5148 <toh5148@psu.edu>
2016-04-04 15:08:33 -0400

    Fix merge conflict

---
+++ botbattle/botbattle/assets/js/QueryStringFunctions.js
@@ -0,0 +1,58 @@
+﻿
+function getChallengeID() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == "cid") {
+            return pair[1];
+            break;
+        }
+    }
+
+    return -1;
+}
+
+function getMatchID() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == "mid") {
+            return pair[1];
+            break;
+        }
+    }
+
+    return -1;
+}
+
+function getMessageID() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == "mes") {
+            return pair[1];
+            break;
+        }
+    }
+
+    return null;
+}
+
+function getUserID() {
+    function getMessageID() {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == "uid") {
+                return pair[1];
+                break;
+            }
+        }
+
+        return -1;
+    }
+}
