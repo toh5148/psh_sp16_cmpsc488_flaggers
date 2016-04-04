a85c060b219bb04fa02887412b67280f51ee4b5e
tus193 <tahahsiddique@gmail.com>
2016-04-04 15:35:56 -0400

    deleted unnecessary code

--- botbattle/botbattle/assets/js/TestingMode/BotUploader.js
+++ botbattle/botbattle/assets/js/TestingMode/BotUploader.js
@@ -1,20 +1,5 @@
 ﻿
-// Create the XHR object used to send CORS calls to the server
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        // CORS not supported.
-        xhr = null;
-    }
-    return xhr;
-}
+
 
 function upload(playerNum) {
     var uploadType = getUploadType(playerNum);
